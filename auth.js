5f9708a190a05e28228cd09492da52b38d887ce5
Plínio Naves <pliniopjn@hotmail.com>
2016-12-04 11:58:47 -0200

    add passport and jwt authentication

---
+++ auth.js
@@ -0,0 +1,41 @@
+import passport from 'passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+module.exports = app => {
+
+    const User = app.db.models.User;
+    const cfg = app.lib.config;
+
+    const params = {
+        secretOrKey: cfg.jwtSecret,
+        jwtFromRequest: ExtractJwt.fromAuthHeader()
+    };
+
+    const strategy = new Strategy(params, (payload, done) => {
+
+        User.findById(payload.id)
+            .then(user => {
+                if (user) {
+                    return done(null, {
+                        id: user.id,
+                        email: user.email
+                    });
+                }
+                return done(null, false);
+            })
+            .catch(error => done(error, null));
+
+    });
+
+    passport.use(strategy);
+
+    return {
+        initialize: () => {
+            return passport.initialize()
+        },
+        authenticate: () => {
+            return passport.authenticate('jwt', cfg.jwtSession)
+        }
+    };
+
+};
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 consign()
     .include('lib/config.js')
     .then('db.js')
+    .then('auth.js')
     .then('lib/middlewares.js')
     .then('routes')
     .then('lib/boot.js')
